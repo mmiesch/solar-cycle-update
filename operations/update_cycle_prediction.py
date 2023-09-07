d9ca5d77af6aec29548e5855ad80680d67c57179
mmiesch <mark.s.miesch@gmail.com>
2023-09-06 18:50:50 -0600

    aspect ratio for "hero" image

--- operations/update_cycle_prediction.py
+++ operations/update_cycle_prediction.py
@@ -356,8 +356,8 @@ month = {
 #------------------------------------------------------------------------------
 # plot SSN
 
-#fig, ax = plt.subplots(2, 1, figsize = [12.8,6.5])
-fig, ax = plt.subplots(2, 1, figsize = [12.8,9.0])
+fig, ax = plt.subplots(2, 1, figsize = [12.8,6.5])
+#fig, ax = plt.subplots(2, 1, figsize = [12.8,9.0])
 
 fig.tight_layout(rect=(0.12,0.18,0.9,.96))
 ax[0].xaxis.set_tick_params(labelsize=14)
@@ -460,8 +460,8 @@ top0 = ax[0].get_position().get_points()[1][1]
 top1 = ax[1].get_position().get_points()[1][1]
 
 xx = .74
-yy = top0 - .06
-dy = .026
+yy = top0 - .05
+dy = .033
 
 ax[0].annotate("International Sunspot Number", (.5,.5), xytext=(xx,yy),xycoords='figure fraction',color='black', ha='center', weight = 'bold')
 yy -= dy
@@ -494,29 +494,29 @@ fig.suptitle("Experimental Solar Cycle 25 Prediction", weight="bold")
 
 hh, ss = ax[1].get_legend_handles_labels()
 
-leg1 = ax[1].legend(hh[0:3],ss[0:3],loc="lower center", bbox_to_anchor=(0.44,-0.514), frameon = False)
+leg1 = ax[1].legend(hh[0:3],ss[0:3],loc="lower center", bbox_to_anchor=(0.44,-0.76), frameon = False)
 
-plt.legend(hh[3:],ss[3:],loc="lower center", bbox_to_anchor=(0.86,-0.59), frameon = False)
+plt.legend(hh[3:],ss[3:],loc="lower center", bbox_to_anchor=(0.86,-0.76), frameon = False)
 ax[1].add_artist(leg1)
 
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
 import matplotlib.image as mpimg
 
 logo = mpimg.imread("noaa-logo-rgb-2022.png")
-imagebox = OffsetImage(logo, zoom = 0.03)
-ab = AnnotationBbox(imagebox, (.13, .12), frameon = False, xycoords='figure fraction', annotation_clip = False)
+imagebox = OffsetImage(logo, zoom = 0.024)
+ab = AnnotationBbox(imagebox, (.135, .13), frameon = False, xycoords='figure fraction', annotation_clip = False)
 ax[1].add_artist(ab)
 
 nwslogo = mpimg.imread("NWS_logo.png")
-imagebox = OffsetImage(nwslogo, zoom = 0.05)
-ab = AnnotationBbox(imagebox, (.19, .12), frameon = False, xycoords='figure fraction', annotation_clip = False)
+imagebox = OffsetImage(nwslogo, zoom = 0.042)
+ab = AnnotationBbox(imagebox, (.185, .13), frameon = False, xycoords='figure fraction', annotation_clip = False)
 ax[1].add_artist(ab)
 
 # creation date
 cdate = datetime.datetime.now()
 clab = f"issued {cdate.day} {month[cdate.month]} {cdate.year}"
-ax[1].annotate("Space Weather Prediction Testbed", (.16,.06),xycoords='figure fraction', ha='center', annotation_clip = False, fontsize = 10)
-ax[1].annotate(clab, (.16,.04),xycoords='figure fraction', ha='center', annotation_clip = False, fontsize = 10)
+ax[1].annotate("Space Weather Prediction Testbed", (.16,.055),xycoords='figure fraction', ha='center', annotation_clip = False, fontsize = 10)
+ax[1].annotate(clab, (.16,.03),xycoords='figure fraction', ha='center', annotation_clip = False, fontsize = 10)
 
 #--------------------
 

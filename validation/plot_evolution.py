6694aecc6984ae888a9fb4f4198c9eca8ba5ecb0
mmiesch <mark.s.miesch@gmail.com>
2023-11-03 12:29:17 -0600

    tweak presentation

--- validation/plot_evolution.py
+++ validation/plot_evolution.py
@@ -5,6 +5,7 @@ Plot some of the results from movie_maker.py to highlight evolution
 import csv
 import datetime
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 import seaborn as sns
 import sys
@@ -67,7 +68,7 @@ with sns.axes_style('whitegrid',{'grid.linestyle': ':', 'axes.facecolor': '#FFF8
 
 plt.rc("font", weight = 'bold')
 
-fig.tight_layout(rect=(0.08,0.08,0.96,0.99))
+fig.tight_layout(rect=(0.08,0.08,0.94,0.98))
 
 sns.set_theme(palette='colorblind')
 
@@ -80,11 +81,28 @@ sns.lineplot(x=pdates,y=amps, color='black', ax = ax1)
 ax1.fill_between(x=pdates, y1 = minamp, y2 = maxamp, color='darkgray', alpha = 0.5)
 
 sns.lineplot(x=pdates,y=maxdates, color='black', ax=ax2)
-ax2.fill_between(x=pdates, y1 = drange1, y2 = drange2, color='darkgray', alpha = 0.5)
+ax2.fill_between(x=pdates, y2 = drange2, y1 = drange1, color='darkgray', alpha = 0.5)
+
+ax2.invert_yaxis()
 
 #------------------------------------------------------------------------------
-# hardwire in axis limits and labels
-# these can be adjusted if necessary
+# indicate release date and most recent prediction
+
+ax1.axhline(y = amps[-1], linestyle=':', color = 'blue')
+ax2.axhline(y = maxdates[-1], linestyle=':', color = 'blue')
+
+release_date = datetime.date(2023,9,15)
+ax1.axvline(x = release_date, linestyle=':', color = 'green')
+ax2.axvline(x = release_date, linestyle=':', color = 'green')
+
+ax1.annotate(f"  {amps[-1]}", (pdates[-1],amps[-1]), ha='left', annotation_clip = False, 
+             fontsize = 10, color = 'blue')
+
+ax2.annotate(f"  {maxdates[-1].month}/{maxdates[-1].year}", (pdates[-1],maxdates[-1]), ha='left', annotation_clip = False, 
+             fontsize = 10, color = 'blue')
+
+#------------------------------------------------------------------------------
+# axis limits and labels
 
 ax1.set_xlim(np.min(pdates),np.max(pdates))
 ax2.set_xlim(np.min(pdates),np.max(pdates))
@@ -93,6 +111,24 @@ ax2.set_xlabel("Prediction Date", weight='bold', fontsize=14)
 
 ax1.set_ylabel("Amplitude (SSN)", weight = 'bold', fontsize = 12)
 ax2.set_ylabel("Date of Max", weight = 'bold', fontsize = 12)
+
+ax1.tick_params(axis='both', which='major', labelsize = 12)
+
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+ax1.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+
+ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+ax2.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+
+ax2.yaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+ax2.yaxis.set_major_locator(mdates.MonthLocator(interval=3))
+
+#------------------------------------------------------------------------------
+# save figure
+
+outfile = outdir + '/prediction_evolution.png'
+
+plt.savefig(outfile)
 
 #------------------------------------------------------------------------------
 plt.show()

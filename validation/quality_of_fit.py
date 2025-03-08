3ab108185363e832911fbb3a7663aa05b98a20f6
mmiesch <mark.s.miesch@gmail.com>
2025-03-08 15:20:25 -0700

    loop over cycles

--- validation/quality_of_fit.py
+++ validation/quality_of_fit.py
@@ -18,69 +18,76 @@ import cycles_util as u
 tmon, d, dsm, t1 = u.get_cycles(tstart = True)
 
 #------------------------------------------------------------------------------
+R_squared = []
+chi_prob = []
 
-cycle = 24
+for cycle in range(6,24):
 
-tobs = tmon[cycle-7]
-ssn = d[cycle-7]
-ssn_sm = dsm[cycle-7]
+  tobs = tmon[cycle-7]
+  ssn = d[cycle-7]
+  ssn_sm = dsm[cycle-7]
 
-afit = curve_fit(u.fuh,tobs,ssn,p0=(170.0,0.0))
-fk = u.fuh(tobs,afit[0][0],afit[0][1])
+  afit = curve_fit(u.fuh,tobs,ssn,p0=(170.0,0.0))
+  fk = u.fuh(tobs,afit[0][0],afit[0][1])
 
-#------------------------------------------------------------------------------
-# R squared value
+  #------------------------------------------------------------------------------
+  # R squared value
 
-resid = ssn - fk
+  resid = ssn - fk
+  ssres = np.sum(resid**2)
+  sstot = np.sum((ssn - np.mean(ssn))**2)
+  r2 = 1.0 - ssres/sstot
+  R_squared.append(r2)
 
-ssres = np.sum(resid**2)
-sstot = np.sum((ssn - np.mean(ssn))**2)
-r2 = 1.0 - ssres/sstot
+  #------------------------------------------------------------------------------
+  # chi squared test
 
-#------------------------------------------------------------------------------
-# chi squared test
+  idx = np.where(ssn > 0.0)
+  y = ssn[idx]
 
-idx = np.where(ssn > 0.0)
-y = ssn[idx]
+  # The stats.chisquare function requres the observed and expected means to be 
+  # the same.  There used to be an option to override this, but apparently 
+  # that has been deprecated
 
-# The stats.chisquare function requres the observed and expected means to be 
-# the same.  There used to be an option to override this, but apparently 
-# that has been deprecated
+  # The standard chi2 adjusts the fit so the means to be the same
+  fexp = fk[idx] - np.mean(fk[idx]) + np.mean(y)
 
-# The standard chi2 adjusts the fit so the means to be the same
-fexp = fk[idx] - np.mean(fk[idx]) + np.mean(y)
+  # but we can do a manual calculation without the adustment
+  fexp_nocheck = fk[idx]
 
-# but we can do a manual calculation without the adustment
-fexp_nocheck = fk[idx]
+  # this is the manual calculation of the chi2 statistic
+  chitest = np.sum((y-fexp)**2/fexp)
+  chitest_nocheck = np.sum((y-fexp_nocheck)**2/fexp_nocheck)
 
-# this is the manual calculation of the chi2 statistic
-chitest = np.sum((y-fexp)**2/fexp)
-chitest_nocheck = np.sum((y-fexp_nocheck)**2/fexp_nocheck)
+  # the degrees of freedom are the number of data points minus the number 
+  # of fitting parameters
+  dof = len(y) - 2
 
-# the degrees of freedom are the number of data points minus the number 
-# of fitting parameters
-dof = len(y) - 2
+  # this is the critical value of the chi2 distribution for 75% confidence
+  # ppf is the percent point function
+  ccrit = chi2.ppf(0.75, df=dof)
 
-# this is the critical value of the chi2 distribution for 75% confidence
-# ppf is the percent point function
-ccrit = chi2.ppf(0.75, df=dof)
+  # This calculates the chi2 statistic and the p-value from scipy
+  csq = chisquare(y, fexp)
+  chi_prob.append(csq.pvalue)
 
-# This calculates the chi2 statistic and the p-value from scipy
-csq = chisquare(y, fexp)
+  # chitest should be the same as csq.statistic
+  print(f'cycle {cycle} ; R^2 = {r2:.2f} dof = {dof}')
+  print(f'chi2: {chitest_nocheck:.2f} {chitest:.2f} {csq.statistic:.2f} crit = {ccrit:.2f} p = {csq.pvalue:.2f}')
 
-# chitest should be the same as csq.statistic
-print(f'chi2: {dof} {chitest_nocheck:.2f} {chitest:.2f} {csq.statistic:.2f} crit = {ccrit:.2f} p = {csq.pvalue:.2f}')
-print(f"R^2 = {r2:.2f}")
 
-# plot the data and the fit
-fig,ax = plt.subplots(1,2,figsize=(12,6))
-ax[0].plot(tobs, ssn, color='black', linewidth=2)
-ax[0].plot(tobs[idx], fexp, color='blue', linewidth=2)
-ax[0].plot(tobs[idx], fexp_nocheck, color='red', linewidth=2)
+  # plot the data and the fit
+  fig,ax = plt.subplots(1,2,figsize=(12,6))
+  ax[0].plot(tobs, ssn, color='black', linewidth=2)
+  ax[0].plot(tobs[idx], fexp, color='blue', linewidth=2)
+  ax[0].plot(tobs[idx], fexp_nocheck, color='red', linewidth=2)
 
-# plot the chi2 distribution
-x = np.linspace(chi2.ppf(0.01,dof),chi2.ppf(0.99,dof))
-ax[1].plot(x, chi2.pdf(x,dof), color='black', linewidth=2)
-ax[1].axvline(chitest, color='blue', linestyle='--')
+  # plot the chi2 distribution
+  x = np.linspace(chi2.ppf(0.01,dof),chi2.ppf(0.99,dof))
+  ax[1].plot(x, chi2.pdf(x,dof), color='black', linewidth=2)
+  ax[1].axvline(chitest, color='blue', linestyle='--')
 
-plt.show()
+  plt.show()
+
+for i in range(0,len(R_squared)):
+  print(f'cycle {i+6} R^2 = {R_squared[i]:.2f} chi2 p = {chi_prob[i]:.2f}')
